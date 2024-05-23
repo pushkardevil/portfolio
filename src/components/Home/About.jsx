f94301e7b688df76fe23eed7f445a980d697ffbc
pushkardevil <20bsm044@iiitdmj.ac.in>
2024-05-23 09:45:38 +0530

    single paragraph edited

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -6,7 +6,7 @@ import {
   AiFillGithub,
   AiFillInstagram,
 } from "react-icons/ai";
-import { FaLinkedinIn, FaDiscord } from "react-icons/fa";
+import { FaLinkedinIn, FaDiscord, FaBluetooth } from "react-icons/fa";
 
 
 const About = () => {
@@ -15,41 +15,39 @@ const About = () => {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+            <h1 style={{ fontSize: "2.6em"}}>
+              Let me Introduce Myself !!
             </h1>
-            <p className="home-about-body">
-              Hi, my name is <span className="yellow">Pushkar Kumar </span>
+            <p className="home-about-body" style={{ color:"blueviolet"}}>
+              Hyy! I am <span className="yellow">Pushkar Kumar </span>
               and I'm from <span className="yellow"> Jabalpur , India</span>
               <br />
               <br />
-              I recently Completed my 3rd year in Bachelor's degree in Mechanical Engineering.
-              <br />
-              <br />
+              Recently I have Completed my Bachelor's degree in the Department of Smart Manufacturing from IIIT Jabalpur.
+              
               As a
-              <b className="yellow"> Full-Stack </b>developer,
+              <b className="yellow"> Full-Stack </b>Developer,
               I enjoy tackling new challenges and continuously expanding my skillset.
               <br />
               <br />
               I have a passion for working
-              with <b className="yellow">Node.js, Machine Learning</b> and
-              <i>
+              with <b className="yellow">Javascript, ReactJs & NodeJs.</b> 
+              {/* <i>
                 <b className="yellow">
                   {" "}
-                  modern Javascript libraries and frameworks
+                  modern Javascript libraries and frameworks.
                 </b>
               </i>
               &nbsp; like
               <i>
                 <b className="yellow"> React.js</b>
-              </i>
-              <br />
+              </i> */}
               <br />I am proficient in
               <b className="yellow"> C++ </b>
-              as well as have knowledge in languages such as Python,
-              <b className="yellow"> SQL, Javascript.</b>
+              as well as have knowledge in languages such as
+              <b className="yellow"> Python, SQL & Javascript.</b>
               <br />
-              <br />I am also interested in building ne
+              <br />I am also interested in building new
               <b className="yellow"> Web Technologies and Products, </b>
               as well as exploring areas related to
               <b className="yellow"> Technology.</b>
